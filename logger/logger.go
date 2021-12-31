0160d96cec34a58bfb6db7c3ec3b160730c043ff
Inhere <in.798@qq.com>
2021-12-31 16:29:59 +0800

    style: add color for debug log

--- logger/logger.go
+++ logger/logger.go
@@ -6,38 +6,40 @@ import (
 	"log"
 
 	"github.com/goft-cloud/go-xxl-job-client/v2/option"
+	"github.com/gookit/color"
 )
 
 // Info log
 func Info(msg string) {
-	log.Println("[XXL-INFO]", msg)
+	log.Println(color.Render("[<info>XXL-INFO</>]"), msg)
 }
 
 // Infof log
 func Infof(tpl string, args ...interface{}) {
-	log.Println("[XXL-INFO]", fmt.Sprintf(tpl, args...))
+	// log.Println("[XXL-INFO]", fmt.Sprintf(tpl, args...))
+	log.Println(color.Render("[<info>XXL-INFO</>]"), fmt.Sprintf(tpl, args...))
 }
 
 // Error log
 func Error(msg string) {
-	log.Println("[XXL-ERROR]", msg)
+	log.Println(color.Render("[<err>XXL-ERROR</>]"), msg)
 }
 
 // Errorf log
 func Errorf(tpl string, args ...interface{}) {
-	log.Println("[XXL-ERROR]", fmt.Sprintf(tpl, args...))
+	log.Println(color.Render("[<err>XXL-ERROR</>]"), fmt.Sprintf(tpl, args...))
 }
 
 // Debug log
 func Debug(msg string) {
 	if option.IsDebugMode() {
-		log.Println("[XXL-DEBUG]", msg)
+		log.Println(color.Render("<lightBlue>[XXL-DEBUG]</>"), msg)
 	}
 }
 
 // Debugf log
 func Debugf(tpl string, args ...interface{}) {
 	if option.IsDebugMode() {
-		log.Println("[XXL-DEBUG]", fmt.Sprintf(tpl, args...))
+		log.Println(color.Render("<lightBlue>[XXL-DEBUG]</>"), fmt.Sprintf(tpl, args...))
 	}
 }
